2d896a8d98dd8b3e34b583533a3a3a6b4107b0d3
miladinovangel <miladinovangel95@gmail.com>
2023-05-26 20:58:47 +0200

    move + jump + dash

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,9 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //1. player-ot avtomatski da se dvizi napred
-    //2. koga ke stisneme levo ili desno na tasta. treba playerot da dashnuva levo i desno
+    [SerializeField] private float speed;
+    [SerializeField] private Rigidbody rbPlayer;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private float dashPower;
+    [SerializeField] private float positionYGravity;
+
+    private void Update()
+    {
+        Jump();
+        Dash();
+    }
+
+    private void FixedUpdate() // koga rabotime so fixed update, ne koristime time.deltaTime
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rbPlayer.MovePosition(transform.position + Vector3.forward * speed);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rbPlayer.AddForce(Vector3.up * jumpPower);
+        }
+    }
+
+    private void Dash()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rbPlayer.AddForce(Vector3.left * dashPower);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rbPlayer.AddForce(Vector3.right * dashPower);
+        }
+    }
 }
+
+//1. player-ot avtomatski da se dvizi napred
+//2. koga ke stisneme levo ili desno na tasta. treba playerot da dashnuva levo i desno
+//3. jump
