4dd16d5f598943dbd6e0cf2aae699bb3e908ea8f
miladinovangel <miladinovangel95@gmail.com>
2023-05-26 20:01:41 +0200

    new task

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //1. player-ot avtomatski da se dvizi napred
+    //2. koga ke stisneme levo ili desno na tasta. treba playerot da dashnuva levo i desno
+}
