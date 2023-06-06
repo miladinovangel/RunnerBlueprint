1557fcab97eba804c03bea8d2db748671446587c
miladinovangel <miladinovangel95@gmail.com>
2023-06-06 20:44:02 +0200

    character package

--- Assets/Scripts/ButtonAnimation.cs
+++ Assets/Scripts/ButtonAnimation.cs
@@ -4,21 +4,41 @@ using UnityEngine;
 
 public class ButtonAnimation : MonoBehaviour
 {
-    private Animator[] anims;
+    //private Animator[] anims;
+    private Animator anim;
 
     private void Start()
     {
-        anims = GetComponentsInChildren<Animator>();
-        Debug.Log(anims.Length);
+        //anims = GetComponentsInChildren<Animator>();
+        //Debug.Log(anims.Length);
+        anim = GetComponentInChildren<Animator>();
     }
 
     public void OnClick()
     {
-        foreach (Animator a in anims)
-        {
-            a.SetTrigger("ButtenClik");
-        }
+        //foreach (Animator a in anims)
+        //{
+        //    a.SetTrigger("ButtenClik");
+        //}
 
-        //anim.SetTrigger("ButtenClik");
+        anim.SetTrigger("ButtenClik");
+        anim.speed = 1.5f;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.speed = 1.5f;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            anim.speed = 1f;
+        }
     }
 }
+
+
+// napravete defaultna animacija na karakterot da bide idle
+// koga ke klikneme na kopceto ArrowUp, treba da se napravi tranzicija kon walk animacija
+// koga ke se pusti kopceto ArrowUp, treba da se vratite na idle animacija
