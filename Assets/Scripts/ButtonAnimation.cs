68e7a87ec7dc7c53a9fe27a656c3959287538873
miladinovangel <miladinovangel95@gmail.com>
2023-06-06 21:02:57 +0200

    task

--- Assets/Scripts/ButtonAnimation.cs
+++ Assets/Scripts/ButtonAnimation.cs
@@ -39,6 +39,6 @@ public class ButtonAnimation : MonoBehaviour
 }
 
 
-// napravete defaultna animacija na karakterot da bide idle
-// koga ke klikneme na kopceto ArrowUp, treba da se napravi tranzicija kon walk animacija
-// koga ke se pusti kopceto ArrowUp, treba da se vratite na idle animacija
+// 1. napravete defaultna animacija na karakterot da bide idle
+// 2. koga ke klikneme na kopceto ArrowUp, treba da se napravi tranzicija kon walk animacija
+// 3. koga ke se pusti kopceto ArrowUp, treba da se vratite na idle animacija
